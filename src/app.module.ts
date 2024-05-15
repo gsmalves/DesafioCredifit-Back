778695fbbd204012c3616c1f140ae85661bac6ef
gsmalves <gustavoeu2017@gmail.com>
2024-05-15 11:34:20 -0300

    atualizanndo o app module com Company e Employee

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity'
-
-import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
+import { Company } from './company/entities/company.entity';
 import { EmployeeModule } from './employee/employee.module';
+import { Employee } from './employee/entities/employee.entity';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { EmployeeModule } from './employee/employee.module';
       port: 5432,
       password: 'admin2024@',
       username: 'postgres',
-      entities: [User],
+      entities: [User, Company, Employee],
       database: 'postgres',
       synchronize: true,
       logging: true,
